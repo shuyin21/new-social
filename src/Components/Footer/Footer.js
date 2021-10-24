596a11e60483009fbbbe7bdd951653566f070d4e
shuyin21 <shuyindev@gmail.com>
2021-10-24 20:47:02 +0100

    Footer elements added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import styled from 'styled-components';
+
 
 const Footer = () => {
     return (
         <>
-
+            <FooterDiv>
+                <FooterWrapper></FooterWrapper>
+            </FooterDiv>
         </>
     )
 }
 
 export default Footer
+
+
+const FooterDiv = styled.div`
+width:100vw;
+height:200px;
+background-color: #222;
+display:flex;
+justify-content:center;
+align-items:center;
+
+`;
+
+const FooterWrapper = styled.div`
+width:60%;
+height:80%;
+
+
+
+`;
