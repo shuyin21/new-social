0bfd1f362c7be270256f8eb68e8ef39a510321e9
shuyin21 <shuyindev@gmail.com>
2021-10-31 19:27:17 +0000

    minor changes

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Slider from 'react-slick';
+
+
 import styled from 'styled-components';
 import sl1 from '../../Images/sl1.jpg';
 import sl2 from '../../Images/sl2.jpg';
@@ -31,7 +33,7 @@ const Gallery = () => {
         dots: false,
         infinite: true,
         speed: 5000,
-        slidesToShow: 2,
+        slidesToShow: 3,
         centerMode: true,
         centerPadding: "60px",
 
@@ -66,19 +68,34 @@ const Gallery = () => {
     return (
         <>
             <MainDiv id='gallery'>
-
                 <CarouselWrapper>
+
                     <Carousel   {...mainSetting}>
+
                         <Wrap>
                             <ImgHolder>
                                 <Img src={sl1} />
+
                             </ImgHolder>
                         </Wrap>
                         <Wrap>
-                            <ImgHolder><ShaderBox></ShaderBox>
-                                <Img src={sl10} />
+                            <ImgHolder>
+                                <Img src={sl2} />
                             </ImgHolder>
                         </Wrap>
+                        <Wrap>
+                            <ImgHolder>
+                                <Img src={sl3} />
+                            </ImgHolder>
+                        </Wrap>
+                        <Wrap>
+                            <ImgHolder>
+                                <Img src={sl4} />
+                            </ImgHolder>
+                        </Wrap>
+
+
+
                     </Carousel>
                 </CarouselWrapper>
             </MainDiv>
@@ -89,116 +106,66 @@ const Gallery = () => {
 export default Gallery
 
 const MainDiv = styled.div`
-
-width: 100vw;
-height:700px;
-display: flex;
-justify-content: center;
-align-items: center;
-background-color: #000;
-
-
-@media screen and (max-width:900px){
-        height:400px;
-        margin:0;
-    }
+width:100vw;
+height:500px;
+display:flex;
+align-items:center;
+justify-content:center;
 `;
 
 const CarouselWrapper = styled.div`
-width: 100vw;
-height:400px;
-display: flex;
-flex-direction: row;
-padding:0 10%;
-
-
-@media screen and (max-width:900px){
-    padding:0 5%;
-    height:200px;
-}
-
-
-
-
-
-
-
+width:100%;
+height:300px;
+display:flex;
+align-items:center;
+justify-content:center;
 `;
 
 
 const Carousel = styled(Slider)`
-
+width: 40%;
+height:300px;
 display:flex;
-flex-direction: row;
-width:100%;
-align-items:center;
-justify-content:center;
-
-
-
-
-
-
 `;
+
 const Wrap = styled.div`
 width:300px;
-padding:0 20px;
-
-background-color: #000;
-
-
-@media screen and (max-width:900px){
-    padding:0 5px;
-}
+height:300px;
 `;
+
 
 
 
 const ImgHolder = styled.div`
 width:300px;
+height:300px;
 border-radius: 4px;
     box-shadow: rgb(0 0 0 /69%) 0px 26px 30px -10px, rgb(0 0 0  / 73%) 0px 16px 10px -10px;
     
     display: block;
    
     
-    height:200px;
-
-border: 5px solid white;
-
-    @media screen and (max-width:900px){
-        height:50%;
-    }
+    height:100%;
+/* box-shadow: rgb(0 0 0 /69%) 0px 26px 30px -10px, rgb(0 0 0  / 73%) 0px 16px 10px -10px; */
 `;
 
 const Img = styled.img`
-
-        width: 100%;
-        height:200px;
-        object-fit: cover;
-z-index:-1;
-       
-
-@media screen and (max-width:900px){
-        height:200px;
-    }
-
-
-
+width:300px;
+height:300px;
+object-fit: cover;
+z-index:100;
 `;
 
-const ShaderBox = styled.div`
-/* width: 100vw;
-  height: 400px;
-  background: rgba(0,0,0,0);
-  position: absolute;
-  display: flex;
-  align-items: flex-end;
-  justify-content: center;
-  text-align: center;
-  z-index: 3;
-  padding:0 20px; */
+// const ShaderBox = styled.div`
+// width: 100vw;
+//   height: 400px;
+//   background: rgba(0,0,0,0);
+//   position: absolute;
+//   display: flex;
+//   align-items: flex-end;
+//   justify-content: center;
+//   text-align: center;
+//   z-index: 3;
+//   padding:0 20px;
+// `;
 
-
-
-`;
