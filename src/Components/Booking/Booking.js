e046c768e84e0d25b419e632dc561dc90e9b18ac
shuyin21 <shuyindev@gmail.com>
2021-11-14 19:15:42 +0000

    changed options

--- src/Components/Booking/Booking.js
+++ src/Components/Booking/Booking.js
@@ -87,9 +87,7 @@ const Booking = () => {
                             <option>4</option>
                             <option>5</option>
                             <option>6</option>
-                            <option>7</option>
-                            <option>8</option>
-                            <option>9</option>
+
 
                         </PersonCount>
 
