804e2df928da18f5df2b4ff91aee06bfd6746fd7
shuyin21 <shuyindev@gmail.com>
2021-11-21 17:56:47 +0000

    logo added to footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,10 +6,13 @@ const Footer = () => {
     return (
         <>
             <FooterDiv>
+                <LogoWrapper />
+
                 <FooterWrapper>
                     <h3>©{(new Date().getFullYear())} SOCIAL BREW CAFE  </h3>
                     <h4>Established London 2017. </h4>
                 </FooterWrapper>
+
             </FooterDiv>
         </>
     )
@@ -35,11 +38,29 @@ display:flex;
 flex-direction: column;
 align-items:center;
 justify-content:center;
+z-index: 150;
 
 
 
 h3,h4{
     color: #fff;
     margin:2px 0;
+    text-shadow: 1px 1px black;
 }
+`;
+
+
+const LogoWrapper = styled.div`
+position:absolute;
+width:100px;
+height:100px;
+display:flex;
+text-align: center;
+
+background-size:cover;
+object-fit: cover;
+background-image: url(${Logo});
+opacity:0.5;
+
+
 `;
